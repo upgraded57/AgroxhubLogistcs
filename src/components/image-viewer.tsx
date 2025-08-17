ae0deb27f22a3868197c7e313ca06762bf6b75b4
upgraded57 <shokunbiupgraded@gmail.com>
2025-08-17 18:40:13 +0100

    update

--- src/components/image-viewer.tsx
+++ src/components/image-viewer.tsx
@@ -1,4 +1,4 @@
-export default function ImageViewer({images, open, handleClose, id}: {images: Array<string>, id: string, open: boolean, handleClose: () => void}) {
+export default function ImageViewer({ id }: { id: string }) {
   return (
     <>
       {/* You can open the modal using document.getElementById('ID').showModal() method */}
@@ -6,12 +6,14 @@ export default function ImageViewer({images, open, handleClose, id}: {images: Ar
         <div className="modal-box">
           <form method="dialog">
             {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
           </form>
           <h3 className="font-bold text-lg">Hello!</h3>
           <p className="py-4">Press ESC key or click on ✕ button to close</p>
         </div>
       </dialog>
     </>
-  )
+  );
 }
